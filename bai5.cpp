ac3a6956faa5a91a908c491938a56ed18779722c
ManhPham <manhdollar2905@gmail.com>
2019-10-04 11:06:33 +0700

    Btvn lần 2

---
+++ bai5.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,m,t=0;
+	printf("Nhap so muon chuyen doi :");
+	scanf("%d",&n);
+	for (;n>0;n/=10)
+	{
+		m=n%10;
+		t=t*10+m;	
+	}
+	printf("So sau khi chuyen doi la: %d",t);
+	
+}
