98506d063cf3cbf93367a3ee65c9bca7791b030c
ManhPham <manhdollar2905@gmail.com>
2019-10-10 18:24:36 +0700

    luyện

--- on4.cpp
+++ on4.cpp
@@ -6,23 +6,27 @@ int main()
 	float x1,x2,x;
 	printf ("Nhap cac he so cua phuong trinh :");
 	scanf ("%d%d%d",&a,&b,&c);
-	if (a==0)
+	if (a==0 &&b==0&&c==0) printf ("\nPt vo so nghiem");
+	else if(a==0 &&b==0) printf ("\nPT vo nghiem");
+	else if (a==0)
     {
 	 x=(float)-c/b;
-	 printf ("Nghiem cua pt la x = %.2f ",x);	
-	}else 
+	 printf ("\nNghiem cua pt la x = %.2f ",x);	
+	}
+	
+	else
 	{
 	    denta=b*b-4*a*c;
-    	if(denta<0) printf ("Phuong trinh vo nghiem ");
+    	if(denta<0) printf ("\nPhuong trinh vo nghiem ");
         else if (denta==0) 
     	{
-		printf ("Phuong trinh co nghiem kep x1 = x2 = %f ",(float)-b/2*a);
+		printf ("\nPhuong trinh co nghiem kep x1 = x2 = %f ",(float)-b/2*a);
 	      }
 	        else 
 	      {
 		x1=(float)(-b+sqrt(denta))/2*a;
 		x2=(float)(-b-sqrt(denta))/2*a;
-		printf ("Phuong trinh co nghiem x1 = %.2f , x2 = %.2f",x1,x2);
+		printf ("\nPhuong trinh co nghiem x1 = %.2f , x2 = %.2f",x1,x2);
      	  }
    }
 	
