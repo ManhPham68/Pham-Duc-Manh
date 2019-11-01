e581506fbbb2c06b98648848435cf7b5c1fe6981
ManhPham <manhdollar2905@gmail.com>
2019-11-01 11:57:41 +0700

    Update 1.practical.cpp

--- 1.practical.cpp
+++ 1.practical.cpp
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int n1=0,n2=1;
-	printf ("Cac so fibonaci tu 1 den 100 la");
-	for (int i=1;i<=100;i++)
+	int n1=0,n2=1,n3;
+	printf ("Cac so fibonaci tu 1 den 100 la :");
+	for (int i=1;n1+n2<100;i++)
 	{
-		int n3=n1+n2;
+	    n3=n1+n2;
 		n1=n2;
 		n2=n3;
-		if (1<=n3 && n3<100) printf ("%d\t",n3);
+	   printf ("%d\t",n3);
 		
 	}
+	return 0;
 }
